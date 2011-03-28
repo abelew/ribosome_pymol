efbc83054d0b512166e04373d05d4ecacaccee4a
Ashton Trey Belew <abelew@gmail.com>
2011-03-28 15:53:36 -0400

    cleaning slightly

--- plug_helices.py
+++ plug_helices.py
@@ -1062,6 +1062,12 @@ cmd.extend("delete_lsu_helices", delete_lsu_helices)
 cmd.extend("delete_ssu_helices", delete_ssu_helices)
 cmd.extend("delete_all_helices", delete_all_helices)
 cmd.extend("get_seq", get_seq)
+
+## Below is Charles Moad's copyright notice for fetch.py
+## Below that, I added the GPLv2, which if it does not conflict
+## I would like to use for the code I wrote.
+
+
 # Copyright Notice
 # ================
 # 
@@ -1091,3 +1097,24 @@ cmd.extend("get_seq", get_seq)
 # OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 # ----------------------------------------------------------------------
+
+
+
+
+
+
+# (gpl-text
+#         "  This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+ 
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+ 
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# ")
